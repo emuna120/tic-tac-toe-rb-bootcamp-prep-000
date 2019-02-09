34c7de82a1946123ddebe295498f26e2c4857870
Amy Man <jwb74fko3ovz@beconfidential.com>
2019-02-09 21:01:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,3 +18,22 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  if user_input.is_a?Array
+    user_input_int = user_input.join.to_i
+  else
+    user_input_int = user_input.to_i
+  end  
+  return -1 if (user_input_int <= 0 || user_input_int > 9 || user_input.length > 1)
+  return (user_input_int - 1)
+end 
+
+def move(board, position, player = "X")
+  
+  if board[position] == " "
+    board[position] = player
+  else
+    puts "position already taken, next player please"
+  end
+  return board
+end  
